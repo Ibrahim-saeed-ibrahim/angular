c54e32e72fae2a5487597cdbfebc7707fbdbdb25
ibrahim <hema.eljoker61@gmail.com>
2026-09-07 17:07:38 +0300

    Refactor Calendar component to use Angular features

--- task-flow-management/src/app/features/calendar/calendar.ts
+++ task-flow-management/src/app/features/calendar/calendar.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../tasks/services/task.service';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './calendar.html',
   styleUrl: './calendar.css',
 })
-export class Calendar {}
+export class Calendar {
+  private taskservice = inject(TaskService);
+  weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  days = Array.from({ length: 30 }, (_, i) => i + 1);
+  tasks = computed(() => this.taskservice.tasksSignal());
+}
