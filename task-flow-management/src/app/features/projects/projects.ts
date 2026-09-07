7efa121cd686c5fbefe1dbe369873450f3e2713f
ibrahim <hema.eljoker61@gmail.com>
2026-09-07 17:06:16 +0300

    Refactor Projects component for standalone and service injection
    Refactor Projects component to use Angular's standalone feature and inject TaskService.

--- task-flow-management/src/app/features/projects/projects.ts
+++ task-flow-management/src/app/features/projects/projects.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../tasks/services/task.service';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
-export class Projects {}
+export class Projects {
+  private taskService = inject(TaskService);
+  projects = [
+    { id: 1, name: 'Smart Home Reception System', description: 'Embedded & IoT automation project' },
+    { id: 2, name: 'Security Audit', description: 'Network security & firewall setup' },
+    { id: 3, name: 'Web Platform', description: 'Angular Standalone dashboard development' }
+  ];
+  getTaskCount(): number {
+    return this.taskService.tasksSignal().length;
+  }
+
+}
