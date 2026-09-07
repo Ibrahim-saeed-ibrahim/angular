3d573b73d1fc07d7af008da9d36934cfbf731239
ibrahim <hema.eljoker61@gmail.com>
2026-09-07 17:05:17 +0300

    Refactor Dashboard component with standalone and injection
    Refactor Dashboard component to use Angular's standalone feature and inject TaskService for task management.

--- task-flow-management/src/app/features/dashboard/dashboard.ts
+++ task-flow-management/src/app/features/dashboard/dashboard.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../tasks/services/task.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
-export class Dashboard {}
+export class DashboardComponent {
+  private taskservive = inject(TaskService);
+  totaltasks = computed(()=>this.taskservive.tasksSignal().length);
+  todocount = computed(()=>this.taskservive.tasksSignal().filter(t=>t.status==='To Do').length);
+  inprogcount = computed(()=>this.taskservive.tasksSignal().filter(t=>t.status==='In Progress').length);
+  donecount = computed(()=>this.taskservive.tasksSignal().filter(t=>t.status==='Done').length);
+
+
+}
